af0c8e002fa9542083d8ef99e4cc54e5a42cdb9d
Daniel <mytechde@outlook.com>
2017-10-20 19:36:55 +0200

    finished http benchmark

--- Controller/HttpController.swift
+++ Controller/HttpController.swift
@@ -7,40 +7,90 @@
 //
 
 import UIKit
+import Alamofire
 
 class HttpController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func getComments(_ sender: Any) {
+        var counter = 0
+        let start = CACurrentMediaTime()
+        for i in 1...50 {
+            Alamofire.request("https://jsonplaceholder.typicode.com/comments/\(i)").responseJSON { response in
+                    counter += 1
+                    if(counter == 50) {
+                        let end = CACurrentMediaTime()
+                        print("GET finished in \(end-start)s")
+                    }
+            }
+        }
     }
     
     @IBAction func postComments(_ sender: Any) {
+        var counter = 0
+        let start = CACurrentMediaTime()
+        for i in 1...50 {
+            let parameters: Parameters = [
+                "id": i,
+                "name": "Lorem Ipsum",
+                "email": "test@mail.com",
+                "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+            ]
+            
+            Alamofire.request("https://httpbin.org/post", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+                counter += 1
+                if(counter == 50) {
+                    let end = CACurrentMediaTime()
+                    print("POST finished in \(end-start)s")
+                }
+            }
+        }
     }
     
     @IBAction func putComments(_ sender: Any) {
+        var counter = 0
+        let start = CACurrentMediaTime()
+        for i in 1...50 {
+            let parameters: Parameters = [
+                "id": i,
+                "name": "Lorem Ipsum",
+                "email": "test@mail.com",
+                "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+            ]
+            
+            Alamofire.request("https://httpbin.org/put", method: .put, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
+                counter += 1
+                if(counter == 50) {
+                    let end = CACurrentMediaTime()
+                    print("PUT finished in \(end-start)s")
+                }
+            }
+        }
     }
     
     @IBAction func deleteComments(_ sender: Any) {
+        var counter = 0
+        let start = CACurrentMediaTime()
+        for i in 1...50 {
+            Alamofire.request("https://jsonplaceholder.typicode.com/comments/\(i)", method: .delete).responseString { response in
+                    counter += 1
+                    if(counter == 50) {
+                        let end = CACurrentMediaTime()
+                        print("DELETE finished in \(end-start)s")
+                    }
+            }
+
+        }
     }
     
 }
